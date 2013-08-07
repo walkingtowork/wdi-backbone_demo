f0935f5879c274c8bdef98492696323b75345e8a
Sumeet Jain <sumeet@sumeetjain.com>
2013-08-07 07:29:51 -0700

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdiBackboneDemo::Application.config.secret_token = '8b4c13787de91b252d84ad6d22727088685d908a35c876049a3e603d3899ad60e8ddb05b16f90f6af0bc1330601b0ff97bd8fa3f06947488e633ee8c8d3b07f5'
